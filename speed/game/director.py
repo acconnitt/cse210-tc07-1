e65fc8df009293736618d3babe9b5caba3bdb42f
benjaminjalebeau <72671113+benjaminjalebeau@users.noreply.github.com>
2021-06-03 11:29:37 -0600

    Update director.py

--- speed/game/director.py
+++ speed/game/director.py
@@ -2,6 +2,8 @@ from time import sleep
 from game import constants
 from game.words import Words
 from game.score import Score
+from game.point import Point
+import random
 # from game.snake import Snake
 
 class Director:
@@ -26,7 +28,8 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
-        self._words = Words()
+        self._word_list = []
+        self.generate_list()
         self._input_service = input_service
         self._keep_playing = True
         self._output_service = output_service
@@ -45,6 +48,21 @@ class Director:
             self._do_outputs()
             sleep(constants.FRAME_LENGTH)
 
+    def generate_list(self):
+        words = open("game/words.txt", "r")
+        list = []
+        positions = [1, 3, 5, 7, 9]
+        for word in words:
+            list.append(word)
+        position = 0
+        for position in positions:
+            next_word = list[random.randint(0,9999)]
+            position += 1
+            self._word_list.append(Words(next_word, position))
+
+        words.close()
+
+
     def _get_inputs(self):
         """Gets the inputs at the beginning of each round of play. In this case,
         that means getting the desired direction and moving the snake.
@@ -53,8 +71,10 @@ class Director:
             self (Director): An instance of Director.
         """
         # direction = self._input_service.get_direction()
-        self._words.move_head(Point(1, 0))
-        pass
+        direction = Point(1,0)
+        for word in self._word_list:
+            word.move_head(direction)
+        
 
     def _do_updates(self):
         """Updates the important game information for each round of play. In 
@@ -75,7 +95,8 @@ class Director:
             self (Director): An instance of Director.
         """
         self._output_service.clear_screen()
-        self._output_service.draw_actor(self._words)
+        
+        self._output_service.draw_actors(self._word_list)
         #self._output_service.draw_actors(self._snake.get_all())
         self._output_service.draw_actor(self._score)
         self._output_service.flush_buffer()
