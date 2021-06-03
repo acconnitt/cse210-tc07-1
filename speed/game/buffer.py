a39e9a0dec728e44f93f6d0debe0f187ebe12f4f
markvagil <83776097+markvagil@users.noreply.github.com>
2021-06-03 11:47:06 -0500

    Update buffer.py

--- speed/game/buffer.py
+++ speed/game/buffer.py
@@ -1,5 +1,13 @@
-
 class Buffer:
 
     def __init__(self):
-        pass
+        self.buffer = ""
+
+    def update_buffer(self, word):
+        self.buffer += str(word)
+
+    def clear_buffer(self):
+        self.buffer = ""
+
+    def get_buffer(self):
+        return self.buffer
