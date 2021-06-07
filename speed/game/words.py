66cf0382b133deca4bbb648f12e64a200495a2e8
acconnitt <41142987+acconnitt@users.noreply.github.com>
2021-06-07 04:15:43 -0400

    Update words.py

--- speed/game/words.py
+++ speed/game/words.py
@@ -2,25 +2,30 @@ import random
 from game.actor import Actor
 from game.point import Point
 
+
 class Words(Actor):
     """
     The Words class puts the words in the words.txt file into a list and returns a word from that list.
     """
 
     def __init__(self, word, y_value):
-        super().__init__()
-        self.set_text(word)
-        self.set_position(Point(random.randint(1,60), y_value))
+        """The class constructor.
+
+        Args:
+            self (Words): an instance of Words.
+        """
+        super().__init__()  # Inheritance
+        self.set_text(word)  # An instance of Actor, a string as parameter
+        # Set a position using ramdom
+        self.set_position(Point(random.randint(1, 60), y_value))
+        self.move_next()  # Moves the actor to its next position according to its velocity
+
+    def move_head(self, direction):  # Funtion that updates the actor's velocity to the given one
+        self.set_velocity(direction)  # The given velocity.
         self.move_next()
 
-    def move_head(self, direction):
-        self.set_velocity(direction)
-        self.move_next()
-        
-    def _get_text(self):
-        return self.get_text()
+    def _get_text(self):  # An instance of Actor.
+        return self.get_text()  # Returns a string
 
-    def _get_position(self):
-        return self.get_position()
-    
-    
+    def _get_position(self):  # An instance of Actor.
+        return self.get_position()  # Returns the position.
