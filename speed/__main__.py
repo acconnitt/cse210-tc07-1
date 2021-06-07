ca09915168c634e7dfe6acdaf5523f8490a9205a
acconnitt <41142987+acconnitt@users.noreply.github.com>
2021-06-07 04:15:11 -0400

    Update __main__.py

--- speed/__main__.py
+++ speed/__main__.py
@@ -1,12 +1,16 @@
 from game.director import Director
 from game.input_service import InputService
 from game.output_service import OutputService
-from asciimatics.screen import Screen 
+from asciimatics.screen import Screen
+
 
 def main(screen):
-    input_service = InputService(screen)
-    output_service = OutputService(screen)
-    director = Director(input_service, output_service)
-    director.start_game()
+    """ Class constructor
+    """
+    input_service = InputService(screen) # An instance of InputService 
+    output_service = OutputService(screen) # An instance of OutputService
+    director = Director(input_service, output_service) # An instance of Director.
+    director.start_game() #An instance of Director.
 
-Screen.wrapper(main)
+
+Screen.wrapper(main) # Initialize the Screen
